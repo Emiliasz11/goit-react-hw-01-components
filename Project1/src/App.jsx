2600337deb134475cfaea93e57a6b6bcbba458a7
EmiliaSzczepaniak52 <emiliaszczepaniak52@gmail.com>
2023-07-23 00:28:17 +0200

    frist commit

---
+++ Project1/src/App.jsx
@@ -0,0 +1,31 @@
+import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+
+import user from '../user.json';
+import data from '../data.json';
+import friendsList from '../data/friends.json';
+import transactions from '../data/transactions.json';
+
+// console.log(user);
+
+export const App = () => {
+  return (
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      <Statistics title="Upload stats" stats={data} />
+
+      <FriendList friends={friendsList} />
+
+      <TransactionHistory items={transactions} />
+    </>
+  );
+};
